ede06b27725369ef089700642219b4b4870634dd
Jason Zeng <595724205@qq.com>
2019-05-20 10:24:33 +0800

    初始化工程

---
+++ DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// 普通单例模式
+    /// </summary>
+    public class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        private static readonly object instancelock = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null) // 不是每次加锁，解决性能问题
+                {
+                    lock (instancelock) // 加锁线程安全
+                    {
+                        if (instance == null) // 再次判断，确保当前线程中实例没有创建
+                        {
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        private static Singleton instance;
+    }
+}
---
+++ DesignPatterns/Singleton/SingletonLazy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// Lazy模式，更少的代码，更优的性能
+    /// </summary>
+    public sealed class SingletonLazy
+    {
+        private SingletonLazy()
+        {
+
+        }
+
+        // 用lazy的方式惰性实例化单例，代码最小，性能最好
+        public static SingletonLazy Instance = new Lazy<SingletonLazy>(() => new SingletonLazy()).Value; 
+    }
+}
